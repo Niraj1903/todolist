5272580a97b826b9231aa3cc2106641a1919eab1
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-11 16:37:25 +0530

    logic for adding toda , deleting todo,marking it as comple

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import "./App.css";
 function App() {
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const handleInputText = (e) => {
-    setInput(e.target.value);
+
+  const addTodoItem = () => {
+    if (input.trim() === "") return;
+    const newItem = {
+      id: todoList.length + 1,
+      item: input,
+      completed: false,
+    };
+    setTodoList((prev) => [...prev, newItem]);
+    setInput("");
   };
 
-  const addTodo = () => {
-    setTodoList((prev) => [...prev, input]);
-    setInput("");
+  const toggleCheckbox = (id) => {
+    setTodoList(
+      todoList.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodoList(todoList.filter((item) => item.id !== id));
   };
 
   return (
@@ -19,15 +41,22 @@ function App() {
         type="text"
         placeholder="Enter your Todo"
         value={input}
-        onChange={(e) => handleInputText(e)}
+        onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={addTodo}>Add</button>
-      {todoList.map((item) => (
-        <>
-          <li key={item}> {item}</li>
-          <button>X</button>
-        </>
-      ))}
+      <button onClick={() => addTodoItem()}>Add</button>
+      <ul>
+        {todoList.map((t) => (
+          <li key={t.id}>
+            <input
+              onChange={() => toggleCheckbox(t.id)}
+              type="checkbox"
+              checked={t.completed}
+            />
+            <span className={t.completed ? "line-through" : ""}>{t.item}</span>
+            <button onClick={() => handleDeleteTodo(t.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
